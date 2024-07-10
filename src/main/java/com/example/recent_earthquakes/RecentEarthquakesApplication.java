6600d85898933f3dc9303c0ed33cc9d210087571
Simay Bakı <simaybaki@Simay-MBP.home>
2024-07-10 22:02:46 +0300

    Initial commit

---
+++ src/main/java/com/example/recent_earthquakes/RecentEarthquakesApplication.java
@@ -0,0 +1,13 @@
+package com.example.recent_earthquakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecentEarthquakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecentEarthquakesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recent_earthquakes/RecentEarthquakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recent_earthquakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecentEarthquakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
